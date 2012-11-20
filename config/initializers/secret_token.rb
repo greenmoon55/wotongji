98206bb7bccc8ed34918fc5072398dda1dbf67bc
greenmoon55 <greenmoon55@gmail.com>
2012-11-21 00:55:02 +0800

    initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wotongji::Application.config.secret_token = '1e46fc369b37804e133e296c989d8d97f4fdb88fc4d410f02813039fafac971e89e0f8b32b1e975414629dd666270f346a78f6a856b8f45809140a8db690dc71'
