e6b4882216d3602465cd62743975acba31f4795f
greenmoon55 <greenmoon55@gmail.com>
2012-11-29 15:05:38 +0800

    add index to users table: email and name

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
 
-  validates :name, presence: true, length: {maximum: 1}
+  validates :name, presence: true, length: {maximum: 14}
+  validates :email, presence: true, uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length: {minimum: 6}
   validates :password_confirmation, presence: true
+
+  before_save { |user| user.email = email.downcase }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121126065142) do
+ActiveRecord::Schema.define(:version => 20121129070330) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -21,5 +21,8 @@ ActiveRecord::Schema.define(:version => 20121126065142) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["name"], :name => "index_users_on_name", :unique => true
 
 end
