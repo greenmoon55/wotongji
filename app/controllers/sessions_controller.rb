84bcce7ff3059f06c82ee5227f755e710b29d408
greenmoon55 <greenmoon55@gmail.com>
2012-12-01 22:34:26 +0800

    sessions helper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,32 +9,9 @@ class SessionsController < ApplicationController
       sign_in user
       redirect_back_or user
     else
-      #flash.now[:error] = "Email或密码错误"
-      render 'new'
+      flash.now[:error] = "Email或密码错误"
+      #render post_signin_path
+      render "new"
     end
   end
-
-
-  def sign_in(user)
-    session[:current_user_id] = user.id
-    self.current_user = user
-  end
-
-  def signed_in?
-    !current_user.nil?
-  end
-
-  def current_user=(user)
-    @current_user = user
-  end
-
-  def current_user
-    @current_user ||= User.find_by_id(session[:current_user_id])
-  end
-
-  def redirect_back_or(default)
-    redirect_to(session[:return_to] || default)
-    session.delete(:return_to)
-  end
-
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,3 +1,25 @@
 # -*- encoding : utf-8 -*-
 module SessionsHelper
+
+	def sign_in(user)
+	  session[:current_user_id] = user.id
+	  self.current_user = user
+	end
+
+	def signed_in?
+	  !current_user.nil?
+	end
+
+	def current_user=(user)
+	  @current_user = user
+	end
+
+	def current_user
+	  @current_user ||= User.find_by_id(session[:current_user_id])
+	end
+
+	def redirect_back_or(default)
+	  redirect_to(session[:return_to] || default)
+	  session.delete(:return_to)
+	end
 end
