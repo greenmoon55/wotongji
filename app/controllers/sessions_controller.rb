a0838e17fec2b2211f8bdfd947419c6809325a02
greenmoon55 <greenmoon55@gmail.com>
2012-12-01 15:38:00 +0800

    signup completed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,39 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by_email(params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_back_or user
+    else
+      #flash.now[:error] = "Email或密码错误"
+      render 'new'
+    end
+  end
+
+
+  def sign_in(user)
+    session[:current_user_id] = user.id
+    self.current_user = user
+  end
+
+  def signed_in?
+    !current_user.nil?
+  end
+
+  def current_user=(user)
+    @current_user = user
+  end
+
+  def current_user
+    @current_user ||= User.find_by_id(session[:current_user_id])
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
 end
