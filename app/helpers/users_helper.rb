6feea00cb059015d7cabdadf7bc0525c1250c663
jingkai.zhao <jingkai.zhao@foxmail.com>
2012-12-14 16:00:45 +0800

    Add gravatar for user

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,3 +1,10 @@
 # -*- encoding : utf-8 -*-
 module UsersHelper
+
+  #use Gravatar to display user image
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
