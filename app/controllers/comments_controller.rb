483c37bd42e000863b09f2184dce1a9f8b01d277
greenmoon55 <greenmoon55@gmail.com>
2012-12-15 17:29:30 +0800

    add hidden field activity_id and modify css

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,13 +5,13 @@ class CommentsController < ApplicationController
   def create
     # Do we need this extra selcect? What about a hidden field?
     # http://stackoverflow.com/questions/6480713/how-to-get-the-post-id-in-rails
-    @activity = Activity.find(params[:activity_id])
-    @comment = @activity.comments.build(params[:comment])
-    @comment.user = current_user
+    #@activity = Activity.find(params[:activity_id])
+    @comment = current_user.comments.build(params[:comment])
+    #@comment.user = current_user
     if @comment.save
       flash[:success] = "评论成功！"
     end
-    redirect_to @activity
+    redirect_to @comment.activity
   end
 
   def destroy
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :activity
   belongs_to :user
-  attr_accessible :content
+  attr_accessible :content, :activity_id
 
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 255 }
