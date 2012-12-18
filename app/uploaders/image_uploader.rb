01f7711862b542c512e0a6b98aa33e75d9b584ad
greenmoon55 <greenmoon55@gmail.com>
2012-12-18 21:19:31 +0800

    custumize filename and add security check

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -42,14 +42,15 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+	def filename
+    #"something.jpg" if original_filename
+		"original.#{model.image.file.extension}" if original_filename
+  end
 
 end
