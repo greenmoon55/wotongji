0f266278ecc3ffe63d84c36331aefe5d06ebfd97
greenmoon55 <greenmoon55@gmail.com>
2012-12-19 21:39:51 +0800

    edit activity

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,6 +1,7 @@
 # -*- encoding : utf-8 -*-
 class ActivitiesController < ApplicationController
   before_filter :require_signin, only: [:new, :create, :destroy]
+	#before_filter :correct_user,   only: [:edit, :update]
 
   def index
     @activities = Activity.paginate(page: params[:page])
@@ -22,6 +23,21 @@ class ActivitiesController < ApplicationController
     else
       flash.now[:error] = "错误"
       render 'new'
+    end
+  end
+  
+  def edit
+    @activity = Activity.find(params[:id])
+  end
+
+  def update
+    @activity = Activity.find(params[:id])
+		if @activity.update_attributes(params[:activity])
+			flash[:success] = "编辑成功！"
+			redirect_to root_url
+    else
+      flash.now[:error] = "错误"
+			render 'new'
     end
   end
 end
