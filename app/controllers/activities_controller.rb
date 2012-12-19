112254601eb196a9d7cd3a43f6861d9da7beb1de
greenmoon55 <greenmoon55@gmail.com>
2012-12-19 22:07:26 +0800

    activity.content format improved and check permission before edit

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,7 +1,7 @@
 # -*- encoding : utf-8 -*-
 class ActivitiesController < ApplicationController
   before_filter :require_signin, only: [:new, :create, :destroy]
-	#before_filter :correct_user,   only: [:edit, :update]
+	before_filter :correct_user,   only: [:edit, :update, :destroy]
 
   def index
     @activities = Activity.paginate(page: params[:page])
@@ -27,7 +27,6 @@ class ActivitiesController < ApplicationController
   end
   
   def edit
-    @activity = Activity.find(params[:id])
   end
 
   def update
@@ -40,4 +39,13 @@ class ActivitiesController < ApplicationController
 			render 'new'
     end
   end
+  
+  private
+    def correct_user
+      @activity = current_user.activities.find_by_id(params[:id])
+      if @activity.nil?
+				flash[:error] = "权限不足"
+				redirect_to Activity.find(params[:id])
+      end
+    end
 end
