b9153436b45d8754f0e9697783012276eda60990
jingkai.zhao <jingkai.zhao@foxmail.com>
2013-01-01 23:01:46 +0800

    fixed activities/index.html.erb and add rounded to gravatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,6 +5,6 @@ module UsersHelper
   def gravatar_for(user, options = {size: 50})
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{options[:size]}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    image_tag(gravatar_url, alt: user.name, class: "avatar img-rounded")
   end
 end
