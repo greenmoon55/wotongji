4936c26038458bd92601ba77686b2260fd476e13
greenmoon55 <greenmoon55@gmail.com>
2013-01-03 13:49:56 +0800

    reply link and username display in textarea

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,10 @@
+function reply(commentId, userName) {
+  reply_body = $("#comment_content");
+  new_text = "@" + userName + " ";
+  if (reply_body.val().trim().length == 0)
+    new_text += '';
+  else
+    new_text = "\n" + new_text;
+    reply_body.focus().val(reply_body.val() + new_text);
+  return false;
+}
