795f6613854f4385ad04c96cb8829d16edefe7ad
greenmoon55 <greenmoon55@gmail.com>
2013-01-03 15:03:47 +0800

    check params[:comment][:parent_id]

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,17 @@ class CommentsController < ApplicationController
     # Do we need this extra selcect? What about a hidden field?
     # http://stackoverflow.com/questions/6480713/how-to-get-the-post-id-in-rails
     #@activity = Activity.find(params[:activity_id])
+    if params[:comment][:parent_id].present?
+      comment = Comment.find(params[:comment][:parent_id])
+      unless comment.nil?
+        if params[:comment][:content].start_with?("@" + comment.user.name)
+          #params[:comment][:content].gsub!("@" + comment.user.name, 
+          #  "@" + view_context.link_to(comment.user.name, comment.user))
+        end
+      end
+    else
+      params[:comment].delete(:parent_id);
+    end
     @comment = current_user.comments.build(params[:comment])
     #@comment.user = current_user
     if @comment.save
