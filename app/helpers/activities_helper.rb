f333182837ce4c86de13c1566443b2bbdf2d1708
greenmoon55 <greenmoon55@gmail.com>
2013-01-08 22:56:38 +0800

    删除活动列表里的圆点，添加删除链接

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -3,6 +3,8 @@ module ActivitiesHelper
   def correct_user_or_admin?
     unless current_user.nil?
       @activity.user == current_user or current_user.admin?
+    else
+      return false
     end
   end
 end
